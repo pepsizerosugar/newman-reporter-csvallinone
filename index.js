1913a8763cc69938b06b22dd1d684923782e7069
pepsizerosugar <juntiger53@gmail.com>
2021-06-12 03:15:22 +0900

    edit parsing requestBody method and etc
    1. edited parsing requestBody method for parsing 'request body type' condition.
    2. edited prasing cURL method for cURL body to same as requestBody
    3. code beauty for better look

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var _         = require('lodash'),
-    progress  = require('cli-progress'),
-    chalk     = require('chalk')
+var _ = require('lodash'),
+  progress = require('cli-progress'),
+  chalk = require('chalk')
 
 let log
 const logs = []
@@ -44,14 +44,15 @@ const CSV = {
 }
 
 /**
- * Reporter that outputs basic logs to CSV (default: newman-run-report.csv).
+ * Reporter that outputs basic logs to CSV (default: $CollectionName-$Date.csv).
  *
  * @param {Object} newman - The collection run object, with event hooks for reporting run details.
  * @param {Object} options - A set of collection run options.
  * @param {String} options.export - The path to which the summary object must be written.
  * @returns {*}
  */
-module.exports = function newmanCSVaioReporter (newman, options) {
+module.exports = function newmanCSVaioReporter(newman, options) {
+  var body
 
   var bar = new progress.Bar({
     format: '== Newman Run Progress |' + chalk.green('{bar}') + '| {percentage}% || Requests: {value}/{total} || ETA: {eta}s ==',
@@ -61,28 +62,32 @@ module.exports = function newmanCSVaioReporter (newman, options) {
   })
 
   newman.on('start', function (err, e) {
-    if (err) { return; }
+    if (err) return
+
     bar.start(e.cursor.length * e.cursor.cycles, 0);
   })
 
   newman.on('beforeItem', (err, e) => {
+    body = ""
+
     if (err) return
 
-    try{
+    try {
       // console.log(JSON.stringify(e))
-    } catch(err){console.log("error parsing case time : " + e.item.name)}
+    } catch (err) { console.log("\nerror parsing case time : " + e.item.name) }
 
     log = {}
   })
 
   newman.on('beforeRequest', (err, e) => {
     if (err || !e.item.name) return
+
     const { cursor, item, request } = e
 
     collName = newman.summary.collection.name
 
     Object.assign(log, {
-      collectionName: newman.summary.collection.name,
+      collectionName: collName,
       iteration: cursor.iteration + 1,
       caseName: item.name,
       requestMethod: request.method,
@@ -91,50 +96,60 @@ module.exports = function newmanCSVaioReporter (newman, options) {
 
     // bodyType check
     try {
-      if(request.hasOwnProperty('body') && JSON.stringify(request.body[request.body.mode]).length > 2){
-        if(request.body.mode === "urlencoded")
-          Object.assign(log, { requestBody: JSON.stringify(request.body[request.body.mode]) })
-        else
-          Object.assign(log, { requestBody: request.body[request.body.mode].toString().replace(/ |\r\n|\r|\n/gi, "") })
-      }
-    } catch (error) {console.log("error : " + item.name + "\n" + JSON.stringify(request))}
+      if (request.hasOwnProperty('body')) {
+        var bodyType = request.body.mode
+        const tempBody = request.body[bodyType]
+
+        if (request.hasOwnProperty('body') && JSON.stringify(tempBody).length > 2) {
+          if (bodyType === "urlencoded" || bodyType === "file" || bodyType === "graphql" || bodyType === "formdata") {
+            body = JSON.stringify(tempBody)
+            Object.assign(log, { requestBody: body })
+          } else {
+            body = tempBody.replace(/ |\r\n|\r|\n/gi, "")
+            Object.assign(log, { requestBody: body })
+          }
+        }
+      } else
+        Object.assign(log, { requestBody: "" })
+    } catch (error) { console.log("\nerror parsing Body Type: " + item.name + "\n" + JSON.stringify(request)) }
 
     // make curl
-    try{
-      var curlUrl, curlHeader, curlBody
-      curlHeader = ""
-      curl = ""
+    try {
+      var curl = "",
+        curlUrl,
+        curlHeader = "",
+        curlBody
+
       curlUrl = "curl --location --request " + e.request.method + " \"" + e.request.url.toString() + "\" \\"
 
       var pointer
       var keyStorage = []
       var valueStorage = []
 
-      if(JSON.parse(JSON.stringify(e.request).toString()).hasOwnProperty('header') != false){
-        pointer = JSON.parse(JSON.stringify(e.request).toString()).header
+      var requestTemp = JSON.stringify(e.request)
 
-        for(var i=0; i < Object.keys(pointer).length; i++){
-          if(pointer.hasOwnProperty('system') !== true){
+      if (JSON.parse(requestTemp.toString()).hasOwnProperty('header') != false) {
+        pointer = JSON.parse(requestTemp.toString()).header
+
+        for (var i = 0; i < pointer.length; i++) {
+          if (pointer.hasOwnProperty('system') !== true) {
             keyStorage.push(JSON.stringify(pointer[i].key))
             valueStorage.push(JSON.stringify(pointer[i].value))
           }
         }
 
-        for(var i=0; i < Object.keys(keyStorage).length; i++)
-          curlHeader += " --header " + "\"" + keyStorage[i].toString("").replace(/\"/gi, "") + ": " + valueStorage[i].toString().replace(/\"/gi, "") + "\" \\"
+        for (var i = 0; i < keyStorage.length; i++)
+          curlHeader += " --header \"" + keyStorage[i].toString("").replace(/\"/gi, "") + ": " + valueStorage[i].toString().replace(/\"/gi, "") + "\" \\"
       }
 
-      if(request.hasOwnProperty('body')){
-        curlBody = " --data \"" + request.body[request.body.mode].toString().replace(/ |\r\n|\r|\n/gi, "").replace(/\"/gi, "\\\"") + "\""
-        if(request.body.mode === "raw")
-          curlHeader += " --header " + "\"Content-Type: application/json\" \\"
+      if (request.hasOwnProperty('body')) {
+        curlBody = " --data \"" + body.replace(/\"/gi, "\\\"") + "\""
         curl += curlUrl + curlHeader + curlBody
-      }
-      else
+      } else
         curl += curlUrl + curlHeader
 
       Object.assign(log, { curl })
-    } catch (error) {console.log("\nerror parsing curl : " + item.name + "\n" + JSON.stringify(request))}
+    } catch (error) { console.log("\nerror parsing cURL : " + item.name + "\n" + JSON.stringify(request)) }
   })
 
   // pre-script collection, folder, case
@@ -143,16 +158,13 @@ module.exports = function newmanCSVaioReporter (newman, options) {
 
     const { executions } = e
 
-    if (JSON.stringify(executions[2]) !== undefined){
-      Object.assign(log, {
-        casePrerequest: JSON.stringify(executions[2].script.exec)
-      })
-    }
-    else{
-      Object.assign(log, {
-        casePrerequest: null
-      })
-    }
+    try {
+      if (JSON.stringify(executions[2]) !== undefined)
+        Object.assign(log, { casePrerequest: JSON.stringify(executions[2].script.exec) })
+      else
+        Object.assign(log, { casePrerequest: null })
+    } catch (err) { console.log("\nerror parsing preRequest") }
+
   })
 
   newman.on('request', (err, e) => {
@@ -160,42 +172,52 @@ module.exports = function newmanCSVaioReporter (newman, options) {
     if (err || !e.item.name) return
 
     const { status, code, responseTime, responseSize, stream } = e.response
-    Object.assign(log, { responseStatus: status, responseCode: code, responseTime, responseSize })
-    Object.assign(log, { responseBody: stream.toString() })
+
+    Object.assign(log, {
+      responseStatus: status,
+      responseCode: code,
+      responseTime,
+      responseSize,
+      responseBody: stream.toString()
+    })
 
     try {
       const headerPointer = JSON.parse(JSON.stringify(e.request)).header
       var headerStorage = []
 
-      for(var i=0; i < headerPointer.length; i++){
-        if(headerPointer[i].hasOwnProperty('system') !== true)
+      for (var i = 0; i < headerPointer.length; i++) {
+        if (headerPointer[i].hasOwnProperty('system') !== true)
           headerStorage.push(headerPointer[i])
       }
       Object.assign(log, { requestHeader: JSON.stringify(headerStorage) })
-    } catch(err) {console.log("error parsing header") }
+    } catch (err) { console.log("\nerror parsing header") }
   })
 
   newman.on('assertion', (err, e) => {
     const { assertion } = e
 
-    const key = err ? 'failedTest' : e.skipped ? 'skippedTest' : 'executedTest'
+    const key = err
+      ? 'failedTest'
+      : e.skipped
+        ? 'skippedTest'
+        : 'executedTest'
 
     log[key] = log[key] || []
     log[key].push(assertion)
 
     try {
-      if(e.hasOwnProperty('error') && e.error !== null){
+      if (e.hasOwnProperty('error') && e.error !== null) {
         const message = e.error.message
-        const key2 = 'assertionMessage'
 
-        log[key2] = log[key2] || []
-        log[key2].push(message.toString().replace(/\'/gi, ""))
+        log['assertionMessage'] = log['assertionMessage'] || []
+        log['assertionMessage'].push(message.toString().replace(/\'/gi, ""))
       }
-    } catch(err) {console.log("error parsing assertion")}
+    } catch (err) { console.log("\nerror parsing assertion") }
   })
 
   newman.on('item', (err, e) => {
     if (err) return
+
     bar.increment();
     logs.push(log)
   })
@@ -204,10 +226,10 @@ module.exports = function newmanCSVaioReporter (newman, options) {
     if (err) return
 
     // timings stats
-    try{
+    try {
       var timings = e.summary.run.timings
       var stats = e.summary.run.stats
-    } catch(err){console.log("error parsing timings")}
+    } catch (err) { console.log("\nerror parsing timings") }
 
     newman.exports.push({
       name: 'newman-csvallinone-reporter',
@@ -221,19 +243,20 @@ module.exports = function newmanCSVaioReporter (newman, options) {
   })
 }
 
-function getResults () {
+function getResults() {
   const results = logs.map((log) => {
     const row = []
 
-    Object.keys(log).forEach((key) => {
-      const val = log[key]
-      const index = columns.indexOf(key)
-      const rowValue = Array.isArray(val)
-        ? val.join('||')
-        : String(val)
+    Object.keys(log)
+      .forEach((key) => {
+        const val = log[key]
+        const index = columns.indexOf(key)
+        const rowValue = Array.isArray(val)
+          ? val.join('||')
+          : String(val)
 
-      row[index] = CSV.stringify(rowValue)
-    })
+        row[index] = CSV.stringify(rowValue)
+      })
 
     return row.join(',')
   })
